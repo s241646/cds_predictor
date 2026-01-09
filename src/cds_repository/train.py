c7ddcfaf38fa5a513b54e2b98bc7efbdcb37b843
Josefien Kam <Josefien.kam@horaizon.co.uk>
2026-01-09 10:37:49 +0100

    Initial commit from cookiecutter

---
+++ src/cds_repository/train.py
@@ -0,0 +1,10 @@
+from cds_repository.model import Model
+from cds_repository.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
