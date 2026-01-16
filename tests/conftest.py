713d8357ab71c90b8082bca54802aa79689c34d1
lsandvad <linesandvad@hotmail.com>
2026-01-16 12:41:37 +0100

    update unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ from tests.utils import make_position_encoded_csv
 
 @pytest.fixture(scope="session")
 def processed_dataloaders(tmp_path_factory):
-    data_dir = tmp_path_factory.mktemp("processed_data/training")
+    data_dir = tmp_path_factory.mktemp("processed_data").joinpath("training")
+    data_dir.mkdir(parents=True, exist_ok=True)
 
     make_position_encoded_csv(data_dir / "train.csv.gz", n=256)
     make_position_encoded_csv(data_dir / "val.csv.gz", n=64)
